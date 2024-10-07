037f5ca493873720df2b95b281943f573f086cfe
Rikler <danielmartinezm15@gmail.com>
2024-10-06 21:01:57 -0500

    Creacion pantalla registro, sincronizacion con menu, foto circular, flujo alterno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.glide)
     implementation(libs.core)
     implementation(libs.gson)
     implementation(libs.picasso)
@@ -70,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    annotationProcessor(libs.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
